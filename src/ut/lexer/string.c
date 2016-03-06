4db4ae9df19b6f9d17d8981207887339810df329
Fhenris3310 <valentin.lahaye@gmail.com>
2016-03-06 14:58:19 +0100

    Debuggage test unitaire string L'ensemble des tests unitaires passent avec succès

--- src/ut/lexer/string.c
+++ src/ut/lexer/string.c
@@ -1,7 +1,15 @@
+/**
+ * \file string.c
+ * \brief Test unitaire du lexer concernant la reconnaissance d'ensembles de type string.
+ * \author LAHAYE Valentin <br>
+ * PARPAITE Thibault
+ * \date 4 Mars 2016
+ */
+
 #include <stdlib.h>
 #include <ut/lexer.h>
 
-
 int main (void) {
-    return (ut_run(UT_NEW("\"string\"", 8, (tokens_t)'"', CHARACTER, CHARACTER, CHARACTER, CHARACTER, CHARACTER, CHARACTER, (tokens_t)'"')) && ut_run(UT_NEW("\"\"\"", 3, (tokens_t)'"', CHARACTER, (tokens_t)'"'))) ? EXIT_SUCCESS : EXIT_FAILURE ;
+    return (ut_run(UT_NEW("\"string\"", 8, (tokens_t)'"', CHARACTER, CHARACTER, CHARACTER, CHARACTER, CHARACTER, CHARACTER, (tokens_t)'"')) &&
+	    ut_run(UT_NEW("\"\\\"\"", 3, (tokens_t)'"', CHARACTER, (tokens_t)'"'))) ? EXIT_SUCCESS : EXIT_FAILURE ;
 }

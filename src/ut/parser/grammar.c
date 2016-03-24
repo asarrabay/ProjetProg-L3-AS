8509d3c03b3aa0325222b6ae50c75e04e671b8d0
emeryk <mr.emeryk@gmail.com>
2016-03-24 14:56:20 +0100

    Ajout possibilité de choisir si le test passe ou non
    On peut maintenant ajouter un troisième argument qui détermine si le test est licite ou non

--- src/ut/parser/grammar.c
+++ src/ut/parser/grammar.c
@@ -4,7 +4,8 @@
  * unitaires sur le parser.
  * \details Le point d'entrée du programme prend en parametre
  * le fichier contenant la grammaire a tester.
- * \author LAHAYE Valentin
+ * \author LABBE Emeric <br>
+ * LAHAYE Valentin
  * \date 14 Mars 2016
  */
 
@@ -13,22 +14,32 @@
 #include <ut/parser.h>
 
 static void usage (const char const *s_program) {
-    printf("usage : %s --help | input\n\t"
-	   "input : path to an existing file on which run the parser\n\t"
-	   "--help : prompt some help for running this program correctly\n", s_program);
+    printf("usage : %s --help | input [OPTIONS]\n\t"
+       "input : path to an existing file on which run the parser\n\t"
+       "--help : prompt some help for running this program correctly\n\t"
+        "OPTIONS :\n\t"
+        "UT_PASSED, UT_FAILED : precise if the ut should be legal or illegal\n", s_program);
     exit(EXIT_FAILURE);
 }
 
 int main (int argc, char *argv[]) {
-    if (argc < 2 || (strcmp(argv[1], "--help") == 0)) {
-	usage(argv[0]);
+    if (argc < 2 || argc > 3 || (strcmp(argv[1], "--help") == 0)) {
+    usage(argv[0]);
+    }
+    ut_status_t e_status = UT_PASSED;
+    if (argc == 3) {
+        if (strcmp(argv[2], "UT_FAILED") == 0) {
+            e_status = UT_FAILED;
+        } else if (strcmp(argv[2], "UT_PASSED") != 0) {
+            usage(argv[0]);
+        }
     }
     FILE *p_input = fopen(argv[1], "r");
     if (p_input == NULL) {
-	printf("An error occured while opening file %s !\n", argv[1]);
-	usage(argv[0]);
+    printf("An error occured while opening file %s !\n", argv[1]);
+    usage(argv[0]);
     }
     ut_status_t status = ut_run(UT_NEW(p_input));
     fclose(p_input);
-    return (status == UT_PASSED) ? EXIT_SUCCESS : EXIT_FAILURE ;
+    return (status == e_status) ? EXIT_SUCCESS : EXIT_FAILURE ;
 }
